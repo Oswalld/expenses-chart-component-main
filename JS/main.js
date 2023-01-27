421b3f72a1b076c72b3361a58844889932cf6707
Oswald Chartrain <72608635+Oswalld@users.noreply.github.com>
2023-01-27 13:42:47 +0100

    project end, js working properly

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,6 @@
-console.log("ehoh");
 window.onload = function(event) {
-// Récupération des données JSON
-const data = [
+  // Récupération des données JSON
+  const data = [
     {
       "day": "mon",
       "amount": 17.45
@@ -31,42 +30,49 @@ const data = [
       "amount": 25.48
     }
   ];
-  
+
   const chartContainer = document.querySelector('.chart');
   let maxAmount = 0;
   let maxAmountIndex = 0;
-  
+
   data.forEach((datapoint, index) => {
-      const chartComponent = document.createElement('div');
-      chartComponent.classList.add('component-chart');
-      
-      const popUp = document.createElement('div');
-      popUp.classList.add('pop-up');
-      chartComponent.appendChild(popUp);
-      
-      const popUpText = document.createElement('p');
-      popUpText.innerText = '$' + datapoint.amount;
-      popUp.appendChild(popUpText);
-  
-      const chartBar = document.createElement('div');
-      chartBar.classList.add('chart-bar', `chart-bar-${index + 1}`);
-      chartBar.style.height += `${datapoint.amount * 3}px`;
-      chartComponent.appendChild(chartBar);
-  
-      const label = document.createElement('p');
-      label.innerText = datapoint.day;
-      chartComponent.appendChild(label);
-  
-      chartContainer.appendChild(chartComponent);
-  
-      if (datapoint.amount > maxAmount) {
-          maxAmount = datapoint.amount;
-          maxAmountIndex = index;
-      }
+    const chartComponent = document.createElement('div');
+    chartComponent.classList.add('component-chart');
+
+    const popUp = document.createElement('div');
+    popUp.classList.add('pop-up');
+    chartComponent.appendChild(popUp);
+
+    const popUpText = document.createElement('p');
+    popUpText.innerText = '$' + datapoint.amount;
+    popUp.appendChild(popUpText);
+
+    const chartBar = document.createElement('div');
+    chartBar.classList.add('chart-bar');
+    chartBar.style.height = `0px`;
+    chartBar.style.transition = 'height 0.5s, opacity 0.1s ease';
+    chartComponent.appendChild(chartBar);
+
+    const label = document.createElement('p');
+    label.innerText = datapoint.day;
+    chartComponent.appendChild(label);
+
+    chartContainer.appendChild(chartComponent);
+
+    if (datapoint.amount > maxAmount) {
+      maxAmount = datapoint.amount;
+      maxAmountIndex = index;
+    }
   });
-  
+
   const chartBars = document.querySelectorAll('.chart-bar');
   chartBars[maxAmountIndex].style.backgroundColor = "hsl(186, 34%, 60%)";
+
+  chartBars.forEach((chartBar, index) => {
+    setTimeout(() => {
+      chartBar.style.height = `${data[index].amount * 3}px`;
+    }, index * 100);
+  });
 
   chartBars.forEach((chartBar) => {
     chartBar.addEventListener("mouseover", () => {
@@ -78,7 +84,4 @@ const data = [
       chartBar.parentNode.firstElementChild.style.display = "none";
     })
   });
-
-
-  
 }
